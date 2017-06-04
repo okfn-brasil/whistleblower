0bc33ad0550ae75290a09db4e1a8fcbc84ce0761
Irio Musskopf <iirineu@gmail.com>
2017-06-04 02:06:30 +0200

    Draft proposal for tasks

--- whistleblower/tasks.py
+++ whistleblower/tasks.py
@@ -1,16 +1,31 @@
+import json
+import logging
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+
+from whistleblower.suspicions import Suspicions
+from whistleblower.targets.twitter import Post as TwitterPost, Twitter
 
 rabbitmq_url = os.environ.get('CLOUDAMQP_URL', 'pyamqp://guest@localhost//')
 app = Celery('tasks', broker=rabbitmq_url)
 
 
 @app.task
-def add(x, y):
-    return x + y
+def enqueue_twitter_posts():
+    reimbursements = Suspicions().all()
+    queue = Twitter().post_queue(reimbursements)
+    logging.info('Queue for Twitter: {} reimbursements'.format(len(queue)))
+    sample = queue.sample(4)
+    for index in range(0, 4):
+        reimbursement = json.loads(sample.iloc[index].to_json())
+        delay = index * 4 * 3600
+        post_reimbursement_to_twitter.apply_async(args=[reimbursement],
+                                                  countdown=delay)
 
 
 @app.task
-def add(x, y):
-    return x + y
+def post_reimbursement_to_twitter(reimbursement):
+    post = TwitterPost(reimbursement)
+    post.publish()
