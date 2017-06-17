440d2198ce49fc27a4acc91f4ecd14add84a7cfe
Irio Musskopf <iirineu@gmail.com>
2017-06-17 14:49:51 +0300

    Create tasks for seting up datasets

--- whistleblower/tasks.py
+++ whistleblower/tasks.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import subprocess
 
 from celery import Celery
 from celery.schedules import crontab
@@ -10,6 +11,25 @@ from whistleblower.targets.twitter import Post as TwitterPost, Twitter
 
 rabbitmq_url = os.environ.get('CLOUDAMQP_URL', 'pyamqp://guest@localhost//')
 app = Celery('tasks', broker=rabbitmq_url)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour=3),
+        update_suspicions_dataset.s()
+    )
+    sender.add_periodic_task(
+        crontab(hour=13),
+        enqueue_twitter_posts.s()
+    )
+
+
+@app.task
+def update_suspicions_dataset():
+    subprocess.run(['python', 'whistleblower/get_dataset.py'], check=True)
+    subprocess.run(['python', 'rosie/rosie.py', 'run',
+                    'chamber_of_deputies', 'data'], check=True)
 
 
 @app.task
